ea92dcc5eb939c6941e4c3ab93166ca19e010c72
Ericka Ward <ericka.ward@gmail.com>
2012-10-18 11:11:20 -0400

    studentclass

---
+++ studentclass.rb
@@ -0,0 +1,51 @@
+#TEST METHODS
+
+def assert_equal(actual, expected)
+  if expected == actual
+    puts 'pass'
+  else
+    puts "fail: expected #{expected}, got #{actual}"
+  end
+end
+
+def assert(statement)
+  if statement
+    puts 'pass'
+  else
+    puts "fail: expected #{statement} to be true."
+  end
+end
+
+#TESTS
+db = SQLite3::Database.open('studentprofiles.sqlite')
+
+class Student
+  attr_accessor :id, :first_name, :last_name, :picture, :bio, :tagline, :email, :blog,
+  :linkedin, :twitter, :github, :codeschool, :coderwall, :stackoverflow, :treehouse
+
+def self.find(student_id)
+  db.execute("SELECT * FROM students WHERE id = ?", student_id)
+end
+
+end
+
+begin
+  assert Student.new.is_a?(Student)
+rescue => e
+  puts e
+end
+
+student = Student.new
+
+[:id, :first_name, :last_name, :picture, :bio, :tagline, :email, :blog,
+  :linkedin, :twitter, :github, :codeschool, :coderwall, :stackoverflow,
+  :treehouse].each do |attribute|
+  student.send("#{attribute}=", "value")
+  assert_equal student.send(attribute), "value"
+end
+
+Student.find(1) #=> #<Student>
+assert Student.find(1).is_a?(Student) 
+
+
+
